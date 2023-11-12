29d2fa4ad9c544c71e5c64da2d0c9b397a948d61
Harry Li <87460010+harrlol@users.noreply.github.com>
2023-11-12 11:18:51 -0500

    new graph

--- scripts/abundByGroup.R
+++ scripts/abundByGroup.R
@@ -10,33 +10,16 @@ cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
 
 args=commandArgs(trailingOnly=TRUE)
 
-abundByLap1 <- function(ldf, lib) {
+abundByGroup <- function(ldf, lib) {
   longdf <- fread(ldf, stringsAsFactors = TRUE)
   longdf%>%
     filter(lap_type==lib)%>%
-    group_by(genotype, seq_tech, mod, .drop = FALSE)%>%
+    group_by(genotype, seq_tech, .drop = FALSE)%>%
     summarise(amount=n())%>%
-    melt(id.vars = c("genotype", "seq_tech","mod"))%>%
-    ggplot(aes(x=genotype, y=value, fill=mod))+
+    melt(id.vars = c("genotype", "seq_tech"))%>%
+    ggplot(aes(x=genotype, y=value))+
     geom_bar(stat = "identity", position = "dodge")+
-    labs(title=paste0("Abundance of HAMR Predicted Modifications in ", lib, " by Sample Groups"))+
-    scale_x_discrete(drop=FALSE, guide = guide_axis(n.dodge=2))+
-    geom_text(aes(label=value), position=position_dodge(width=0.9), vjust=-0.25)+
-    facet_wrap(~seq_tech)+
-    scale_fill_manual(values=cbPalette)
-}
-
-## not used 
-abundByLap2 <- function(ldf, lib) {
-  longdf <- fread(ldf, stringsAsFactors = TRUE)
-  longdf%>%
-    filter(lap_type==lib)%>%
-    group_by(genotype, seq_tech, mod, .drop = FALSE)%>%
-    summarise(amount=n())%>%
-    melt(id.vars = c("genotype", "seq_tech","mod"))%>%
-    ggplot(aes(x=mod, y=value, fill=genotype))+
-    geom_bar(stat = "identity", position = "dodge")+
-    labs(title=paste0("Abundance of HAMR Predicted Modifications in ", lib, " by Mod Type"))+
+    labs(title=paste0("Total Abundance of HAMR Predicted Modifications in ", lib, " by Sample Groups"))+
     scale_x_discrete(drop=FALSE, guide = guide_axis(n.dodge=2))+
     geom_text(aes(label=value), position=position_dodge(width=0.9), vjust=-0.25)+
     facet_wrap(~seq_tech)+
@@ -53,6 +36,6 @@ all_annotations <- a[grep("bed", a, ignore.case = TRUE)]
 for (ant in all_annotations) {
   segs <- strsplit(ant, "_")[[1]]
   lap_type <- sub("\\..*", "", segs[length(segs)])
-  abundByLap1(args[1], lap_type)
-  ggsave(paste0(args[3],"/mod_abundance_",lap_type,".png"), width = 10, height = 8, units = "in")
+  abundByGroup(args[1], lap_type)
+  ggsave(paste0(args[3],"/mod_abundance_by_group_",lap_type,".png"), width = 10, height = 8, units = "in")
 }
